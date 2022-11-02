78dd7588682b1fc459eaf849a506765f6a36901a
Chen-Kuan Liao <czl5899@psu.edu>
2022-11-01 20:13:03 -0400

    getting rid of the button

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 var commands = document.getElementById("commands");
-var commandline = document.getElementById("commandline");
-var temp_btn = document.getElementById("temp_btn");
+var comuser_inputmandline = document.getElementById("user_input");
 
 temp_btn.addEventListener("click", function(){
     var newCommand = document.createElement("li");
@@ -9,3 +8,22 @@ temp_btn.addEventListener("click", function(){
     commandline.value = "";
 });
 
+function execute(input){
+    var newCommand = document.createElement("li");
+    newCommand.innerHTML = input;
+    commands.appendChild(newCommand);
+}
+
+function key(e){
+    const input = user_input.innerHTML;
+
+    if(e.key == "Enter"){
+        execute(input);
+        user_input.value = "";
+        return;
+    }
+
+    user_input.innerHTML = input + e.key;
+}
+
+document.addEventListener("keypress", key);
